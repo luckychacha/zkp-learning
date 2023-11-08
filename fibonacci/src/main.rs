a5180ab427e2a2d1f6802c5c4905767d1aa5d937
luckychacha <358140284@qq.com>
2023-11-08 18:33:14 +0800

    Fibi init.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,17 @@
+use halo2_proofs::pasta::group::ff;
+use halo2_proofs::plonk::{Advice, Column, Selector};
+
+#[derive(Debug, Clone)]
+struct FiboConfig {
+    pub advice: [Column<Advice>; 3],
+    pub selector: Selector,
+}
+
+struct FiboChip<F: ff::Field> {
+    config: FiboConfig,
+    _marker: std::marker::PhantomData<F>,
+}
+
 fn main() {
     println!("Hello, lesson 2: Fibonacci!");
 }
