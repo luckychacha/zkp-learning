bfce26c903136c6449c7163643420a7d3b80068a
luckychacha <358140284@qq.com>
2023-11-10 23:54:43 +0800

    Use a better struct name.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -2,13 +2,14 @@ use halo2_proofs::pasta::group::ff;
 use halo2_proofs::plonk::{Advice, Column, Selector};
 
 #[derive(Debug, Clone)]
-struct FiboConfig {
+struct FibonacciConfig {
     pub advice: [Column<Advice>; 3],
     pub selector: Selector,
 }
 
+#[derive(Debug, Clone)]
 struct FiboChip<F: ff::Field> {
-    config: FiboConfig,
+    config: FibonacciConfig,
     _marker: std::marker::PhantomData<F>,
 }
 
