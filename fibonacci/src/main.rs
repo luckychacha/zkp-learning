24481dbf087ee7e626de526f40fb8221bf5dbb7f
luckychacha <358140284@qq.com>
2023-11-11 23:59:58 +0800

    Add some logic.

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,5 +1,9 @@
+use halo2_proofs::arithmetic::Field;
+use halo2_proofs::circuit::{Layouter, SimpleFloorPlanner};
 use halo2_proofs::pasta::group::ff;
-use halo2_proofs::plonk::{Advice, Column, Selector};
+use halo2_proofs::plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Selector};
+use halo2_proofs::poly::Rotation;
+use std::alloc::Layout;
 
 #[derive(Debug, Clone)]
 struct FibonacciConfig {
@@ -8,9 +12,83 @@ struct FibonacciConfig {
 }
 
 #[derive(Debug, Clone)]
-struct FiboChip<F: ff::Field> {
+struct FibonacciChip<F: ff::Field> {
     config: FibonacciConfig,
     _marker: std::marker::PhantomData<F>,
+}
+
+impl<F: ff::Field> FibonacciChip<F> {
+    fn construct(config: FibonacciConfig) -> Self {
+        Self {
+            config,
+            _marker: std::marker::PhantomData,
+        }
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>, advice: [Column<Advice>; 3]) -> FibonacciConfig {
+        let [col_a, col_b, col_c] = advice;
+        let selector = meta.selector();
+
+        meta.enable_equality(col_a);
+        meta.enable_equality(col_b);
+        meta.enable_equality(col_c);
+
+        meta.create_gate("add", |meta| {
+            let s = meta.query_selector(selector);
+            let a = meta.query_advice(col_a, Rotation::cur());
+            let b = meta.query_advice(col_b, Rotation::cur());
+            let c = meta.query_advice(col_c, Rotation::cur());
+            vec![s * (a + b - c)]
+        });
+
+        FibonacciConfig {
+            advice: [col_a, col_b, col_c],
+            selector,
+        }
+    }
+
+    fn assign_first_row(&self, mut layouter: impl Layouter<F>, a: Option<F>, b: Option<F>) {
+        layouter.assign_region(
+            || "first row",
+            |mut region| {
+                self.config.selector.enable(&mut region, 0)?;
+                let a_cell = region.assign_advice(
+                    || "a",
+                    self.config.advice[0],
+                    0,
+                    || a.or_then(Error::Synthesis),
+                )?;
+            },
+        )
+    }
+}
+
+#[derive(Default)]
+struct MyCircuit<F> {
+    pub a: Option<F>,
+    pub b: Option<F>,
+}
+
+impl<F: ff::Field> Circuit<F> for MyCircuit<F> {
+    type Config = FibonacciConfig;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        let col_a = meta.advice_column();
+        let col_b = meta.advice_column();
+        let col_c = meta.advice_column();
+        FibonacciChip::configure(meta, [col_a, col_b, col_c])
+    }
+
+    fn synthesize(&self, config: Self::Config, layouter: impl Layouter<F>) -> Result<(), Error> {
+        let chip = FibonacciChip::construct(config);
+
+        chip.assign_first_row(layouter.namespace(|| "assign first row"), self.a, self.b);
+    }
 }
 
 fn main() {
